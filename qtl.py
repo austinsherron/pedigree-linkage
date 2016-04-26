240be40ccf96fafd05c885a9b478065ef65e0894
Bert Austin Sherron <mail@austinsherron.com>
2016-04-25 23:49:19 -0700

    Major Updates to QTL and UAI Classes
    QTL/UAI now output (almost) correct pedigree-linkage UAI files.

--- qtl.py
+++ qtl.py
@@ -4,31 +4,22 @@ import numpy as np
 import sys
 
 from networkx import DiGraph,Graph
-from networkx import topological_sort
+from networkx import connected_component_subgraphs,topological_sort
 from pedigree import Pedigree
-
-
-"""
-NOTE: it's assumed that the variables are named using the following
-convention: i_s/i_d for i in range(# of variables). This should 
-probably be more general.
-"""
 
 
 class QTL:
 
-    def __init__(self, ped, qtl_file=None, allele_file=None, num_alleles=30):
+    def __init__(self, ped, qtl_file, allele_file, num_alleles=30):
         self.ped = ped
+        self.len = num_alleles
 
-        if qtl_file:
-            self.build_allele_assignments(qtl_file, num_alleles)
-    
-        if allele_file:
-            self.get_allele_info(allele_file, num_alleles)
-            self.build_allele_graph()
+        self.build_allele_assignments(qtl_file, self.len)
+        self.get_allele_info(allele_file, self.len)
+        self.build_allele_graph(self.len)
 
 
-    def print_allele_network(self):
+    def show_bayes_net(self):
         G = self.allele_graph
         nx.draw(G)
         plt.show()
@@ -48,10 +39,11 @@ class QTL:
             print(p)
 
 
-    def get_allele_info(self, allele_file, lim=30):
+    def get_allele_info(self, allele_file, num_alleles=None):
+        num_alleles = num_alleles if num_alleles else self.len
         allele_info = []
         for i,r in enumerate(self._file_iter(allele_file)):
-            if i == lim:
+            if i == num_alleles:
                 break
 
             allele_info.append([a.split(':')[1] for a in r[4:]])
@@ -60,40 +52,44 @@ class QTL:
         return allele_info
 
 
-    def build_allele_assignments(self, qtl_file, lim=30):
+    def build_allele_assignments(self, qtl_file, num_alleles=None):
+        num_alleles = num_alleles if num_alleles else self.len
         allele_assigns = {}
         for r in self._file_iter(qtl_file):
-            id,l = r[0],r[1:lim + 1]
+            id,l = r[0],r[1:(num_alleles * 2) + 1]
             allele_assigns[id] = [(l[i],l[i + 1]) for i in range(0, len(l) - 1, 2)]
 
         self.allele_assigns = allele_assigns
         return allele_assigns
 
         
-    def build_allele_graph(self):
+    def build_allele_graph(self, num_alleles=None):
+        num_alleles = num_alleles if num_alleles else self.len
         G = DiGraph()
 
-        for k,v in self.ped.pedigree.items():
-            G.add_node(self.mk_nd(k, 'd'))
-            G.add_node(self.mk_nd(k, 's'))
+        for i in range(num_alleles):
+            for k,v in self.ped.pedigree.items():
+                G.add_node(self.mk_nd(k, 1, i))
+                G.add_node(self.mk_nd(k, 0, i))
 
-        for k,v in self.ped.pedigree.items():
-            for c in v['cs']:
-                type = 's' if v['sex'] == 'm' else 'd'
-                G.add_edge(self.mk_nd(k, 'd'), self.mk_nd(c, type))
-                G.add_edge(self.mk_nd(k, 's'), self.mk_nd(c, type))
-                G.add_edge(self.mk_nd('S', k, c), self.mk_nd(c, type))
+            for k,v in self.ped.pedigree.items():
+                for c in v['cs']:
+                    type = 0 if v['sex'] == 'm' else 1 
+                    G.add_edge(self.mk_nd(k, 1, i), self.mk_nd(c, type, i))
+                    G.add_edge(self.mk_nd(k, 0, i), self.mk_nd(c, type, i))
+                    G.add_edge(self.mk_nd('S', k, c, i), self.mk_nd(c, type, i))
+                    
+                    if i > 0:
+                        G.add_edge(self.mk_nd('S', k, c, i - 1), self.mk_nd('S', k, c, i))
 
+        self.vars = topological_sort(G)
+        self.var_idxs = dict([(v,i) for i,v in enumerate(self.vars)])
         self.allele_graph = G
         return G
 
 
     def mk_nd(self, *args):
         return '_'.join(map(str, args))
-        
-        
-    def index(self, x): 
-        return (int(x[:-2]) * 2 + (0 if x[-1] == 'd' else 1)) - 1
 
 
     def _file_iter(self, file):
@@ -104,6 +100,44 @@ class QTL:
                 yield l
 
 
+    def __getitem__(self, var):
+        if type(var) is tuple:
+            split_var = var[0].split('_')
+            var,i = var
+            return split_var[i]
+        else:
+            return var.split('_')
+
+
+    def sort_vars(self, vars):
+        srtd = sorted(vars, key=lambda x: self.var_idxs[x])
+        for i,var in enumerate(srtd):
+            if self[var,0] == 'S':
+                    srtd.pop(i)
+                    srtd.insert(0, var)
+        return srtd
+
+
+    def index(self, var):
+        return self.var_idxs[var]
+
+
+    def card(self, var):
+        if self[var,0] == 'S':
+            return 2
+        else:
+            return len(self.allele_info[int(self[var,2])])
+
+
+    def allele_val(self, var, index=False):
+        id,prnt,depth = tuple(map(int, self[var]))
+        val = self.allele_assigns[str(id)][depth][prnt]
+        if index:
+            return int(val) - 1
+        else:
+            return val
+
+
 if __name__ == '__main__':
 
     qtl_file = sys.argv[1] if len(sys.argv) > 1 else '../data/test_qtl.txt'
@@ -111,9 +145,6 @@ if __name__ == '__main__':
     ped_file = sys.argv[3] if len(sys.argv) > 3 else '../data/test_data.txt'
 
     ped = Pedigree(ped_file)
-    qtl = QTL(ped, qtl_file=qtl_file, allele_file=allele_file)
-#   print(qtl.allele_assigns)
-#    print(qtl.allele_info)
+    qtl = QTL(ped, qtl_file=qtl_file, allele_file=allele_file, num_alleles=1)
     qtl.print_bayes_factors()
-    qtl.print_allele_network()
 
--- uai.py
+++ uai.py
@@ -1,111 +1,173 @@
+import argparse as ap
 import numpy as np
 import sys
 
-from linkage import Linkage
+from pedigree import Pedigree
+from qtl import QTL
 from networkx import dfs_tree,find_cliques,topological_sort
 from random import random as rand
 
 
-class UAI(Linkage):
+class UAI:
 
-    def __init__(self, mrk_file=None, ped_file=None):
-        Linkage.__init__(self, mrk_file, ped_file)
+    def __init__(self, qtl):
+        self.qtl = qtl
 
 
     def write(self):
-        """
-        NOTE: it's assumed that valid cliques are of size 3
-        """
-        G = self.allele_graph
+        G = self.qtl.allele_graph
 
         print('MARKOV')
-        print(len(self.allele_graph))
-        print(' '.join([str(len(self.allele_types)) for v in self.allele_graph]))
-        self._print_clique_assigns()
-        self._print_factors()
+        print(len(G))
+        self._print_cardinalities()
+        print(len(G))
+        self._print_cliques(G)
+        self._print_factors(G)
 
 
-    def _print_clique_assigns(self):
-        uG = self.tri_graph
-        cliques = 0
-        out = []
-        for clique in find_cliques(uG):
-            if len(clique) == 3:
-                vars = [str(self.index(c)) for c in clique]
-                vars.sort(key=lambda x: int(x))
-                out.append(((len(clique), ' '.join(vars))))
-                cliques += 1
-
-        print(cliques)
-        for clique_len,clique in out:
-            print(clique_len, clique)
-
-
-    def _print_factors(self):
-        uG = self.tri_graph
-        for clique in find_cliques(uG):
-            cl = len(clique) 
-            if cl == 3:
-                print(len(self.allele_types) ** cl)
-                self._build_factor(cl)
-
-
-    def _build_factor(self, cl):
-        factor = np.zeros((len(self.allele_types),) * cl)
-        at = len(self.allele_types)
-
-        for i in range(at):
-            for j in range(at):
-                factor[i][j][i] += 0.5
-                factor[i][j][j] += 0.5
-
-        for i in range(at):
-            for j in range(at):
-                for k in range(at):
-                    print(factor[i][j][k], end=' ')
-
+    def _print_cardinalities(self):
+        vars = self.qtl.vars
+        for var in vars:
+            print(self.qtl.card(var), end=' ')
         print()
 
 
-    def observe(self, prob=0.5):
-        observed = {}
-        nodes = topological_sort(self.allele_graph)
-        num_nodes = len(nodes)
-        for i,node in enumerate(nodes, 1):
-            if rand() < prob * (i / num_nodes):
-                if self.allele_network[node[:-2]][node[-1]] == None:
-                    continue
+    def _print_cliques(self, G):
+        vars = self.qtl.vars
+        for i,var in enumerate(vars):
+            clique = self.qtl.sort_vars([var] + G.predecessors(var))
+            print(len(clique), end=' ')
+            print(' '.join([str(self.qtl.index(v)) for v in clique]))
 
-                if self.allele_graph.in_degree(node) == 0 and rand() < 0.05:
-                    val = self.allele_network[node[:-2]][node[-1]]
-                    observed[self.index(node)] = val
-                elif self.allele_graph.in_degree(node) > 0:
-                    val = self.allele_network[node[:-2]][node[-1]]
-                    observed[self.index(node)] = val
+
+    def _print_factors(self, G):
+        vars = self.qtl.vars
+
+        for i,var in enumerate(vars):
+            clique = self.qtl.sort_vars([var] + G.predecessors(var))
+
+            if len(clique) == 1 and self.qtl[var,0] != 'S':
+                self._print_founder(clique[0])
+            elif len(clique) == 1 and self.qtl[var,0] == 'S':
+                self._print_seg_node()
+            elif len(clique) == 4:
+                self._print_non_founder(clique)
+            elif len(clique) == 2 and self.qtl[var,0] == 'S':
+                self._print_seg_clique()
+            else:
+                raise AssertionError('UAI._print_factors: unrecognized clique type')
+
+
+    def _print_founder(self, var):
+        allele_idx = int(self.qtl[var,2])
+        print(len(self.qtl.allele_info[allele_idx]))
+        for prob in self.qtl.allele_info[allele_idx]:
+            print(prob, end=' ')
+        print()
+
+
+    def _print_seg_node(self):
+        print(2)
+        print(0.5, 0.5)
+
+
+    def _print_seg_clique(self):
+        print(4)
+        print(0.5, 0.5, 0.5, 0.5)
+
+
+    def _print_non_founder(self, clique):
+        al     = self.qtl.allele_info[int(self.qtl[clique[1],2])]
+        al_len = len(al)
+        father = 1 if int(self.qtl[clique[1],1]) == 0 else 2
+        factor = self._build_factor(father, al_len)
+        print(len(factor))
+        print(' '.join(map(str, factor)))
+
+
+    def _build_factor(self, father, al_len):
+        factor = []
+        for i in range(2):
+            for j in range(al_len):
+                for k in range(al_len):
+                    for l in range(al_len):
+                        if i == 0 and father == 1 and j == l:
+                            factor.append(1.0)
+                        elif i == 0 and father == 2 and k == l:
+                            factor.append(1.0)
+                        elif i == 1 and father == 2 and j == l:
+                            factor.append(1.0)
+                        elif i == 1 and father == 1 and k == l:
+                            factor.append(1.0)
+                        else:
+                            factor.append(0.0)
+
+        return factor
+
+
+    def observe(self, prob=0.5, see=None):
+        observed = self._build_observed(prob, see)
 
         print(len(observed), end=' ')
         for var,val in observed.items():
             print(var, val, end=' ')
         print()
 
+    
+    def _build_observed(self, prob=0.5, see=None):
+        observed = {}
+        if not see:
+            see = lambda *args: rand() < args[0] * (args[1] / args[2])
+
+        for i,var in enumerate(self.qtl.vars):
+            if not see(prob, i, len(self.qtl.vars)):
+                continue
+
+            if self.qtl[var,0] == 'S':
+                continue
+
+            observed[self.qtl.index(var)] = self.qtl.allele_val(var, index=True)
+
+        return observed
+
+
+    def _observe_seg_var(self, var):
+        prnt         = self.qtl[var,1]
+        id           = self.qtl[var,2]
+        sex          = self.qtl.ped.pedigree[prnt]['sex']
+        idx          = 0 if sex == 'm' else 1
+        child_allele = self.allele_assigns[id][idx]
+        parent_alls  = self.allele_assigns[prnt]
+
         
-    def _dfs(n, G, depth, visited=set()):
-        visited.add(n)
-        print('node', n, 'at depth', depth)
-        for succ in G.successors(n):
-            if succ not in visited:
-                UAI._dfs(succ, G, depth + 1, visited)
-
-
 if __name__ == '__main__':
 
-    action   = sys.argv[1] if len(sys.argv) > 1 else 'gen'
-    mrk_file = sys.argv[2] if len(sys.argv) > 2 else '../data/test_qtl.txt'
-    ped_file = sys.argv[3] if len(sys.argv) > 3 else '../data/test_data.txt'
+    parser = ap.ArgumentParser()
+    parser.add_argument('qtl_file')
+    parser.add_argument('allele_file')
+    parser.add_argument('ped_file')
+    parser.add_argument('-no', '--network_output_file')
+    parser.add_argument('-eo', '--evid_output_file')
+    parser.add_argument('-na', '--num_alleles')
+    parser.add_argument('-po', '--percent_to_observe')
+    args = parser.parse_args()
 
-    uai = UAI(mrk_file=mrk_file, ped_file=ped_file)
+    ped = Pedigree(args.ped_file)
+    num = int(args.num_alleles) if args.num_alleles else 1
+    qtl = QTL(ped, args.qtl_file, args.allele_file, num)
+    uai = UAI(qtl)
 
-    if action == 'gen':
+    net_out_file = args.network_output_file if args.network_output_file else 'out.uai'
+    with open(net_out_file, 'w') as f:
+        sys.stdout = f
         uai.write()
-    elif action == 'observe':
-        uai.observe(0.8)
+
+    evid_out_file = args.evid_output_file if args.evid_output_file else 'out.uai.evid'
+    with open(evid_out_file, 'w') as f:
+        sys.stdout = f
+        uai.observe()
+
+    with open('var_assigns', 'w') as f:
+        sys.stdout = f
+        print(qtl.vars)
+        print(qtl.allele_assigns)
