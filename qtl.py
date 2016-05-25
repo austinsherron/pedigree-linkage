ec9b489e9411f79ebea796de5b066f87755a0d1d
Bert Austin Sherron <mail@austinsherron.com>
2016-05-25 07:54:50 -0700

    Multiple Changes

--- qtl.py
+++ qtl.py
@@ -3,14 +3,14 @@ import networkx as nx
 import numpy as np
 import sys
 
-from networkx import DiGraph,Graph
+from networkx import ancestors,DiGraph,Graph
 from networkx import connected_component_subgraphs,topological_sort
 from pedigree import Pedigree
 
 
 class QTL:
 
-    def __init__(self, ped, qtl_file, allele_file, num_alleles=30):
+    def __init__(self, ped, qtl_file, allele_file, pos_file, num_alleles=1):
         """
         Constructor for QTL class. The QTL class relates QTL (quantitative
         trait loci) data to pedigree data: this class relates allele assigments
@@ -43,6 +43,15 @@ class QTL:
             are needed). Chr refers to the chromosome the allele is found on.
             For each row, there should be a probability for each potential
             allele assignment.
+        pos_file : str
+            The name of a file that specifies allele (locus) positions on
+            the chromosome. Allele files have rows with the following formats:
+
+                ID (str) Chr (int) Pos (float)
+
+            Chr refers to the chromosome ID on which a particular allele is
+            found. Pos refers to the allele's (locus's) position on the 
+            chromosome; this number is normally a float between 0 and 100.
         num_alleles : int (optional)
             The number of alleles to include. Should not be larger than the
             number of alleles available in the specified file.
@@ -52,6 +61,7 @@ class QTL:
 
         self.build_allele_assignments(qtl_file, self.len)
         self.get_allele_info(allele_file, self.len)
+        self.get_allele_positions(pos_file, self.len)
         self.build_allele_graph(self.len)
 
 
@@ -81,6 +91,22 @@ class QTL:
             print(p)
 
 
+    def print_var_info(self):
+        """
+        Method that prints info about variable assignments.
+        """
+        fmt_str = "{:>10}{:>12}{:>8}{:>10}{:>16}{:>14}"
+        print(fmt_str.format('UAI Index', 'Name', 'Allele', 'Var Type', 
+            'Inherited From', 'Allele Index'))
+        for v,i in self.var_idxs.items():
+            type = 'seg' if self[v,0] == 'S' else 'allele'
+            if type == 'seg':
+                print(fmt_str.format(i, v, 'n/a', type, 'n/a', 'n/a'))
+            else:
+                in_from = 's' if self[v,1] == '0' else 'd'
+                print(fmt_str.format(i, v, self[v,0], type, in_from, self[v,2]))
+
+
     def get_allele_info(self, allele_file, num_alleles=None):
         """
         Method that parses allele files and stores allele assignments
@@ -106,6 +132,28 @@ class QTL:
 
         self.allele_info = allele_info
         return allele_info
+
+
+    def get_allele_positions(self, pos_file, num_alleles=None):
+        """
+        Method that get allele position info from pos_file. See constructor 
+        for parameter descriptions.
+
+        Returns
+        -------
+        allele_positions : [float]
+            List that maps alleles (indexes) to their positions on the 
+            chromosome.  allele_position[1] = the position on the chromosome
+            of allele 2.
+            
+        """
+        num_alleles = num_alleles if num_alleles else self.len
+        allele_positions = []
+        for r in self._file_iter(pos_file):
+            allele_positions.append(float(r[2]))
+
+        self.allele_positions = allele_positions
+        return allele_positions
 
 
     def build_allele_assignments(self, qtl_file, num_alleles=None):
@@ -342,14 +390,86 @@ class QTL:
             return val
 
 
+    def recomb_prob(self, allele_1, allele_2):
+        """
+        Method that calculates the recombination fraction using the map 
+        distance of two alleles.
+
+        Parameters
+        ----------
+        allele_1 : literal (int)
+            Index of an allele to use for the calculation.
+        allele_2 : literal (int)
+            Same as above, but allele_1 != allele_2.
+
+        Returns
+        -------
+        float 
+            The recombination fraction of the two allele provided.
+        """
+        pos_1 = self.allele_positions[int(allele_1)]
+        pos_2 = self.allele_positions[int(allele_2)]
+        return np.round(0.5 * (1 - np.e**(-2 * np.abs(pos_1 - pos_2))), 6)
+
+
+    def extract_sub_pedigree(self, num_founders=5, gens=10, offspring=2, start_gen=0):
+        """
+        Method that extracts a sub
+        """
+        G = self.allele_graph
+        last_gen = [v for v in self.vars if G.in_degree(v) == 0 and self[v,0] != 'S']
+
+        if len(last_gen) < num_founders:
+            raise ValueError('QTL.extract_sub_pedigree: number of founders is less than num_founders (' + num_founders + ')')
+
+        last_gen = set(last_gen[:num_founders])
+        new_G    = DiGraph()
+        # for each desired generation
+        for i in range(gens):
+            print(i, len(last_gen))
+            # keep track of next gen
+            next_gen = set()              
+            # for each parent in the last gen
+            for var in last_gen:
+                print('parent =', var)
+                # add parent to graph
+                new_G.add_node(var)
+                # find children of current parent
+                children = set(G.successors(var)[:offspring])
+                next_gen |= children
+                # for each child of that parent
+                for child in children:
+                    print('child =', child)
+                    # find the missing parent
+                    missing_parents = G.predecessors(child)
+                    # for each missing parent (there will only be one)
+                    for missing_parent in missing_parents:
+                        print('missing parent =', missing_parent)
+                        # add to the graph
+                        new_G.add_node(missing_parent)
+            last_gen = next_gen
+
+        for node in new_G.nodes():
+            if node not in last_gen:
+                for child in G.successors(node):
+                    new_G.add_edge(node, child)
+        
+        self.vars = topological_sort(new_G)
+        self.var_idxs = dict([(v,i) for i,v in enumerate(self.vars)])
+        self.allele_graph = new_G
+        return new_G
+
+
 if __name__ == '__main__':
 
-    qtl_file = sys.argv[1] if len(sys.argv) > 1 else '../data/test_qtl.txt'
+    qtl_file    = sys.argv[1] if len(sys.argv) > 1 else '../data/test_qtl.txt'
     allele_file = sys.argv[2] if len(sys.argv) > 2 else '../data/test_alleles.txt'
-    ped_file = sys.argv[3] if len(sys.argv) > 3 else '../data/test_data.txt'
+    ped_file    = sys.argv[3] if len(sys.argv) > 3 else '../data/test_data.txt'
+    pos_file    = sys.argv[4] if len(sys.argv) > 4 else '../data/test_pos.txt'
 
     ped = Pedigree(ped_file)
-    qtl = QTL(ped, qtl_file=qtl_file, allele_file=allele_file, num_alleles=1)
-#qtl.print_bayes_factors()
+    qtl = QTL(ped, qtl_file, allele_file, pos_file)
+    print(qtl.allele_positions)
     print(qtl.allele_info)
-
+    print(qtl.vars)
+    print(qtl.var_idxs)
--- uai.py
+++ uai.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import argparse as ap
 import numpy as np
 import sys
@@ -12,23 +14,43 @@ class UAI:
 
     def __init__(self, qtl):
         """
-        Constructor for the UAI class. This is the class 
+        Constructor for the UAI class. This is the class that generates UAI
+        from a QTL class with a fully formed segregation network.
+
+        Parameters
+        ----------
+        qtl : QTL object
+            Fully constructed QTL object that contains a well formed 
+            segregation graph. 
         """
         self.qtl = qtl
 
 
     def write(self):
+        """
+        Container method responsible for writing UAI model files.
+        """
         G = self.qtl.allele_graph
 
+        # header prefix (model type)
         print('MARKOV')
+        # print number of variables
         print(len(G))
+        # cardinalities
         self._print_cardinalities()
+        # number of factors (== to number of variables)
         print(len(G))
+        # variable -> clique assignments
         self._print_cliques(G)
+        # factor tables
         self._print_factors(G)
 
 
     def _print_cardinalities(self):
+        """
+        Method that prints the cardinalities of variables in the segregation 
+        network.
+        """
         vars = self.qtl.vars
         for var in vars:
             print(self.qtl.card(var), end=' ')
@@ -36,52 +58,118 @@ class UAI:
 
 
     def _print_cliques(self, G):
+        """
+        Method that prints the variables clique assignments.
+
+        Parameters
+        ----------
+        G : networkx DiGraph
+            nx.DiGraph representation of a segregation network.
+        """
         vars = self.qtl.vars
         for i,var in enumerate(vars):
+            # vars in clique are sorted according to special ordering defined
+            # in QTL.sort_vars method; special ordering involves segregation
+            # nodes coming before allele nodes; this is to make printing factor
+            # tables easier
             clique = self.qtl.sort_vars([var] + G.predecessors(var))
             print(len(clique), end=' ')
             print(' '.join([str(self.qtl.index(v)) for v in clique]))
 
 
     def _print_factors(self, G):
+        """
+        Method that prints factor tables of cliques.
+
+        Parameters
+        ----------
+        G : networkx DiGraph
+            nx.DiGraph representation of a segregation network.
+        """
         vars = self.qtl.vars
 
         for i,var in enumerate(vars):
             clique = self.qtl.sort_vars([var] + G.predecessors(var))
 
+            # founder allele 
             if len(clique) == 1 and self.qtl[var,0] != 'S':
                 self._print_founder(clique[0])
+            # founder segregation node
             elif len(clique) == 1 and self.qtl[var,0] == 'S':
                 self._print_seg_node()
+            # non-founder clique
             elif len(clique) == 4:
                 self._print_non_founder(clique)
+            # non-founder segregation clique
             elif len(clique) == 2 and self.qtl[var,0] == 'S':
-                self._print_seg_clique()
+                self._print_seg_clique(clique)
             else:
-                raise AssertionError('UAI._print_factors: unrecognized clique type')
+                print(clique)
+                raise AssertionError('UAI._print_factors: unrecognized clique type of size ' + str(len(clique)))
 
 
     def _print_founder(self, var):
+        """
+        Method that prints the probability table for a founder node (clique).
+
+        Parameter
+        ---------
+        var : str
+            String name of a founder node in a segregation network.         
+        """
+        # get index of allele
         allele_idx = int(self.qtl[var,2])
         print(len(self.qtl.allele_info[allele_idx]))
+        # print empirical probabilities for allele assignments
         for prob in self.qtl.allele_info[allele_idx]:
             print(prob, end=' ')
         print()
 
 
-    def _print_seg_node(self):
+    def _print_seg_node(self, prob=0.5):
+        """
+        Method that prints probabilities for founder segregation node.
+
+        Parameters
+        ----------
+        prob : float (optional)
+            Probability that sire allele segregates.
+        """
         print(2)
-        print(0.5, 0.5)
+        print(prob, 1 - prob)
 
 
-    def _print_seg_clique(self):
+    def _print_seg_clique(self, clique):
+        """
+        Method that prints probabilities for non-founder segregation clique.
+        """
         print(4)
-        print(0.5, 0.5, 0.5, 0.5)
+        allele_1 = self.qtl[clique[0],3]
+        allele_2 = self.qtl[clique[1],3]
+        recomb_frac = self.qtl.recomb_prob(allele_1, allele_2)
+        for i in range(2):
+            for j in range(2):
+                if i == j:
+                    print(1 - recomb_frac, end=' ')
+                else:
+                    print(recomb_frac, end=' ')
+        print()
 
 
     def _print_non_founder(self, clique):
+        """
+        Method that prints probability table for non-founder clique.
+
+        Parameters
+        ----------
+        clique : [str]
+            List of string var names in segregation network.
+        """
+        # figure out which allele this clique describes
         al     = self.qtl.allele_info[int(self.qtl[clique[1],2])]
         al_len = len(al)
+        # index of allele inherited from father in factor table depends on 
+        # whether this clique describes a sire allele or a dam allele
         father = 1 if int(self.qtl[clique[1],1]) == 0 else 2
         factor = self._build_factor(father, al_len)
         print(len(factor))
@@ -89,17 +177,43 @@ class UAI:
 
 
     def _build_factor(self, father, al_len):
+        """
+        Method that builds factors table for non-founder clique.
+
+        Parameters
+        ----------
+        father : int
+            Index of parent allele inherited from father.
+        al_len : int
+            Number of possible allele assignments.
+
+        Returns
+        -------
+        factor : [scalar]
+            Factor table for a non-founder clique.
+        """
         factor = []
+        # for each possible assignment of the segregation node
+        #   i = 0 means allele inherited from father segregated
+        #   i = 1 means same for allele inherited from dam
         for i in range(2):
+            # for each possible assignment of one parent allele
             for j in range(al_len):
+                # for each possible assignment of the other parent allele
                 for k in range(al_len):
+                    # for each possible assignment of the inherited allele
                     for l in range(al_len):
+                        # determine probability of assignment...
+                        # inherited sire allele
                         if i == 0 and father == 1 and j == l:
                             factor.append(1.0)
+                        # inherited sire allele
                         elif i == 0 and father == 2 and k == l:
                             factor.append(1.0)
+                        # inherited dam allele
                         elif i == 1 and father == 2 and j == l:
                             factor.append(1.0)
+                        # inherited dam allele
                         elif i == 1 and father == 1 and k == l:
                             factor.append(1.0)
                         else:
@@ -109,6 +223,30 @@ class UAI:
 
 
     def observe(self, prob=0.5, see=None):
+        """
+        Container method responsible for writing UAI evidence files.
+
+        Parameters
+        ----------
+        prob : float (optional)
+            Partial probability of observing a node.  Real probablity starts
+            at zero and increases to value of prob as _build_observed moves
+            through a topological sort of the seg graph's nodes. This founder
+            nodes from being observed too often.
+        see : function (optional)
+            Function that determines real probability of observing nodes. Function
+            takes three paramters:
+                prob : float
+                    Base probability of observing node.
+                i : int
+                    Index of variable being considered for observation in a 
+                    topological sort. 
+                total : int
+                    Total number of nodes in seg graph.
+            If no function is provided, the following is used:
+                prob to be observed = prob * (i / total)
+            Nodes "higher" in a toplogical sort are less likely to be observed.
+        """
         observed = self._build_observed(prob, see)
 
         print(len(observed), end=' ')
@@ -118,17 +256,30 @@ class UAI:
 
     
     def _build_observed(self, prob=0.5, see=None):
+        """
+        Method that build map of variables and their real values. See observe
+        doc string for descriptions of parameters.
+
+        Returns
+        -------
+        observed = {literal: literal}
+            Dictionary that maps variables to their true assignments.
+        """
         observed = {}
         if not see:
             see = lambda *args: rand() < args[0] * (args[1] / args[2])
 
+        # for each variable in a topological sort of the seg graph
         for i,var in enumerate(self.qtl.vars):
+            # determine whether or not to observe the variable
             if not see(prob, i, len(self.qtl.vars)):
                 continue
 
+            # TODO: can't currently observe segregation nodes
             if self.qtl[var,0] == 'S':
                 continue
 
+            # find true value of var and record it
             observed[self.qtl.index(var)] = self.qtl.allele_val(var, index=True)
 
         return observed
@@ -149,15 +300,19 @@ if __name__ == '__main__':
     parser.add_argument('qtl_file')
     parser.add_argument('allele_file')
     parser.add_argument('ped_file')
+    parser.add_argument('pos_file')
     parser.add_argument('-no', '--network_output_file')
     parser.add_argument('-eo', '--evid_output_file')
     parser.add_argument('-na', '--num_alleles')
     parser.add_argument('-po', '--percent_to_observe')
+    parser.add_argument('-va', '--var_assigns')
     args = parser.parse_args()
 
     ped = Pedigree(args.ped_file)
     num = int(args.num_alleles) if args.num_alleles else 1
-    qtl = QTL(ped, args.qtl_file, args.allele_file, num)
+    wva = args.var_assigns
+    qtl = QTL(ped, args.qtl_file, args.allele_file, args.pos_file, num)
+    qtl.extract_sub_pedigree()
     uai = UAI(qtl)
 
     net_out_file = args.network_output_file if args.network_output_file else 'out.uai'
@@ -170,7 +325,7 @@ if __name__ == '__main__':
         sys.stdout = f
         uai.observe()
 
-    with open('var_assigns', 'w') as f:
-        sys.stdout = f
-        print(qtl.vars)
-        print(qtl.allele_assigns)
+    if wva:
+        with open('var_assigns', 'w') as f:
+            sys.stdout = f
+            qtl.print_var_info()
